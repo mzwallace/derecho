6e1003ef3e66cb648020b3aaad31f1380bc4e137
David Merrique <david.merrique@gmail.com>
2013-03-11 10:51:55 -0400

    Creates and deletes tmp directory before and after tests.

--- spec/derecho/config_spec.rb
+++ spec/derecho/config_spec.rb
@@ -3,44 +3,48 @@ require 'derecho/config'
 
 describe Derecho do
 
-  let(:hash) { {'test' => {'a' => 'a_test', 'b' => 'b_test', 'c' => 'c_test'}} } 
+  let(:hash) { {'test' => {'a' => 'a_test', 'b' => 'b_test', 'c' => 'c_test'}} }
   let(:path) { 'spec/tmp/.test' }
 
   before :each do
     yaml = hash.to_yaml.sub('---', '')
-    File.open(File.expand_path(path), 'w+') {|f| f.write(yaml) }
+    dir = File.dirname(path)
+    Dir.mkdir(dir) unless Dir.exists?(dir)
+    File.open(path, 'w+') {|f| f.write(yaml) }
     @config = Derecho::Config.new(hash)
   end
-  
-  after :each do 
-    File.delete(path) if File.exists?(path) 
+
+  after :each do
+    File.delete(path) if File.exists?(path)
+    dir = File.dirname(path)
+    Dir::delete(dir) if Dir.exists?(dir)
   end
 
   describe "::Config" do
-    
-    it "should be an instance of Derecho::Config" do 
+
+    it "should be an instance of Derecho::Config" do
       @config.should be_an_instance_of Derecho::Config
     end
-    
+
     it "should initialize correctly when no arguments are passed" do
       @config = Derecho::Config.new
       @config.settings.should be_empty
     end
-    
+
     it "should initialize correctly when a String is passed as the argument" do
       expect(@config.settings).to eq(YAML.load_file(File.expand_path(path)))
     end
-    
-    it "should initialize correctly when a Hash is passed as the argument" do 
+
+    it "should initialize correctly when a Hash is passed as the argument" do
       @config = Derecho::Config.new(hash)
       expect(@config.settings).to eq(hash)
     end
-    
+
     it "should raise error when no file is found" do
-      File.delete(path) if File.exists?(path)   
+      File.delete(path) if File.exists?(path)
       lambda { Derecho::Config.new(path) }.should raise_error
     end
-    
+
     it "should be accessible like a Hash" do
       expect(@config['test']['a']).to eq('a_test')
       @config['test']['a'] = 'testing'
